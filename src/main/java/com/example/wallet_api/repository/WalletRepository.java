227754b4bb77d6d1ad1a785c4de228b3e1768282
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-09 20:04:19 +0200

    findByUserId method is created

--- src/main/java/com/example/wallet_api/repository/WalletRepository.java
+++ src/main/java/com/example/wallet_api/repository/WalletRepository.java
@@ -3,6 +3,9 @@ package com.example.wallet_api.repository;
 import com.example.wallet_api.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Optional<Wallet> findByUserId(Long userId);
 
 }
