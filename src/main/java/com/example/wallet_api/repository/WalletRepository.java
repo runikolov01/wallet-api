4e99e44258c21815b292c7f246b569ec21b30ed8
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-15 00:35:19 +0200

    WalletRepository is updated

--- src/main/java/com/example/wallet_api/repository/WalletRepository.java
+++ src/main/java/com/example/wallet_api/repository/WalletRepository.java
@@ -3,9 +3,9 @@ package com.example.wallet_api.repository;
 import com.example.wallet_api.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-    Optional<Wallet> findByUserId(Long userId);
 
+    List<Wallet> findByUserId(Long userId);
 }
