55fa481f23828bcc49d3319ff5819d181dc51545
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-15 00:33:32 +0200

    UserController class is created

---
+++ src/main/java/com/example/wallet_api/web/UserController.java
@@ -0,0 +1,48 @@
+package com.example.wallet_api.web;
+
+import com.example.wallet_api.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/home")
+    public String home(Model model, HttpSession session) {
+        return userService.openHomePage(model, session);
+    }
+
+    @GetMapping("/register")
+    public String registerForm(Model model, HttpSession session, HttpServletRequest request) {
+        return userService.openRegisterForm(model, session, request);
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm(@RequestParam(required = false) String error, Model model, HttpSession session) {
+        return userService.openLoginForm(error, model, session);
+    }
+
+    @PostMapping("/register")
+    public String registerSubmit(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String pin, @RequestParam String city, @RequestParam int telephoneNumber, @RequestParam String email, @RequestParam String password, @RequestParam String confirmPassword, RedirectAttributes redirectAttributes, Model model) {
+        return userService.registerProcess(firstName, lastName, pin, city, telephoneNumber, email, password, confirmPassword, redirectAttributes, model);
+    }
+
+
+    @PostMapping("/login")
+    public String loginSubmit(@RequestParam String email, @RequestParam String password, Model model, HttpSession session) {
+        return userService.loginProcess(email, password, model, session);
+    }
+}
