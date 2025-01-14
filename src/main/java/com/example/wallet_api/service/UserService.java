0c5dea807607fa5b1c4ad65345f8f96e62d98d67
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-15 00:37:12 +0200

    UserService is updated

--- src/main/java/com/example/wallet_api/service/UserService.java
+++ src/main/java/com/example/wallet_api/service/UserService.java
@@ -29,7 +29,7 @@ public interface UserService {
     boolean registerUser(User user);
 
     String registerProcess(String firstName, String lastName, String pin, String city, int telephoneNumber, String email, String password, String confirmPassword, RedirectAttributes redirectAttributes, Model model);
-    
+
     String loginProcess(String email, String password, Model model, HttpSession session);
 
     Optional<User> getUserById(Long userId);
