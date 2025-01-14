1576bf8a714ced26f61c7e308bc4b8125fdc8d29
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-15 00:34:48 +0200

    WalletService interface is updated

--- src/main/java/com/example/wallet_api/service/WalletService.java
+++ src/main/java/com/example/wallet_api/service/WalletService.java
@@ -1,7 +1,25 @@
 package com.example.wallet_api.service;
 
+import com.example.wallet_api.model.Wallet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Service
-public class WalletService {
+public interface WalletService {
+
+    List<Wallet> getWalletsByUserId(Long userId);
+
+    String openCreateWalletForm(Model model, HttpSession session);
+
+    void createWallet(Long userId, String walletName, String currency);
+
+    Wallet getWalletById(Long walletId);
+
+    void depositMoney(Long walletId, double amount);
+
+    void withdrawMoney(Long walletId, double amount);
 }
